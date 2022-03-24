f7e98a380a97dfd38222a6c010613b4dff6edb76
NackiE23 <dimavarchuc@gmail.com>
2022-03-24 15:19:49 +0200

    clear

--- backups/24.03.2022/electronic_journal/journal/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from .views import *
-
-urlpatterns = [
-    path('', main, name="main"),
-    path('create_new_user', create_new_user, name="create_new_user")
-]
