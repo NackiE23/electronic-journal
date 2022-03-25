b7f7559dc81d59bccfd705099e1430d06b2ae7d2
NackiE23 <dimavarchuc@gmail.com>
2022-03-25 17:03:19 +0200

    Register custom user in admin panel

--- electronic_journal/journal/admin.py
+++ electronic_journal/journal/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from .forms import UserAdminChangeForm, UserAdminCreationForm
+
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserInAdmin(UserAdmin):
+    """ All User Admin Model (Include Super User) """
+    # The forms to add and change user instances
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+
+    search_fields = ['email', 'name', 'surname', 'is_admin', 'is_staff', 'is_active']
+    list_display = ['email', 'is_admin', 'is_staff', 'is_active']
+    list_filter = ['is_admin', 'is_staff', 'is_active']
+
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password', ('name', 'surname'),)
+        }),
+        ('Contact', {
+            # 'classes': ('collapse',),
+            'fields': ('phone_number',)
+        }),
+        ('Biographical Details', {
+            # 'classes': ('collapse',),
+            'fields': ('avatar',)
+        }),
+        ('Permissions', {
+            'fields': ('is_admin', 'is_staff', 'is_active')
+        }),
+        # ('Group Permissions', {'fields': ('groups', 'user_permissions')}), if add permissions class
+    )
+
+    add_fieldsets = (
+        (None, {
+            # 'classes': ('wide',),
+            'fields': ('name', 'surname', 'email', 'password1', 'password2')}
+         ),
+    )
+
+    ordering = ('email',)
+    filter_horizontal = ()
